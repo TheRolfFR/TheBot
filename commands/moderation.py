8cdc477f669c9dfb097ed98be2e9d231dae8a48f
repl.it user <replituser@example.com>
2022-01-28 22:06:25 +0000

    Reworked clear command

--- commands/moderation.py
+++ commands/moderation.py
@@ -12,7 +12,11 @@ async def cmd_clear(bot, message, command, args):
 
     ds_role = discord.utils.get(message.guild.roles, name="lvl 30 SNAIL")
 
-    if ds_role not in message.author.roles:
+    is_admin = message.author.guild_permissions.administrator or ds_role in message.author.roles
+
+    is_admin = is_admin or discord.utils.get(message.guild.roles, name="Admin Service") in message.author.roles
+
+    if not is_admin:
         await message.channel.send(
             embed=discord.Embed(
                 title=f"Aide sur {command}",
@@ -22,21 +26,12 @@ async def cmd_clear(bot, message, command, args):
         )
         return
 
-    if len(args) != 1:
+    number = 10
+    if len(args) > 0:
+      if not args[0].isdigit():
         await message.channel.send(embed=bot.doc_embed("clear", ERROR_COLOR))
         return
-    if not args[0].isdigit():
-        await message.channel.send(embed=bot.doc_embed("clear", ERROR_COLOR))
-        return
-
-    number = int(args[0])
+      number = int(args[0])
+    
     await message.channel.purge(limit=number + 1)
-    alert = await message.channel.send(
-        embed=discord.Embed(
-            color=CONFIRM_COLOR,
-            description=f":x: **``{number}`` messages supprimé(s)** :x:",
-        )
-    )
-    await asyncio.sleep(2)
-    await alert.delete()
 
